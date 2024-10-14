efa13f3e5bc6f01b3081edbbb7ef2a61db146c72
Your Name <you@example.com>
2024-10-15 00:55:50 +0100

    CarSales

--- Car.java
+++ Car.java
@@ -1,56 +1,118 @@
-class Car
+// Car class Definition
+public class Car
 
-    {
-        private static int startNumber = 100;
+    {// Begin Car class
+        private static int startNumber = 1000;
         private int saleNumber;
-        private String carName;
         private String makeName;
         private String modelName;
         private String carType;
         private double engineSize;
         private double salePrice;
+        private Dealership assignedDealership;
+        int carCount;
 
-        public Car()
+        public Car(String carMake, String carModel, double size, double price, Dealership dealership)
         {
             setSaleNumber(startNumber);
             startNumber++;
+            carCount = 1000-startNumber;
+            makeName = carMake;
+            modelName = carModel;
+            engineSize = size;
+            salePrice = price;
+            assignedDealership = dealership;
         }
 
         //Set Methods
-        //
-
+        // set the sale number
         public void setSaleNumber(int number)
         {
             saleNumber = number;
         }
 
-        public void setName(String name)
-        {
-            carName = name;
-        }
-
+        // set the car make
         public void setMake(String name)
         {
             makeName = name;
         }
 
+        // set the car model
         public void setModel(String name)
         {
             modelName = name;
         }
 
+        // set the car type
         public void setType(String name)
         {
             carType = name;
         }
 
+        // set the engine size
         public void setEngineSize(double size)
         {
             engineSize = size;
         }
 
+        // set the sale price
         public void setPrice(double price)
         {
             salePrice = price;
         }
-    }
+        //Get Methods
+        // get the car make
+        public String getMake()
+        {
+            return makeName;
+        }
+
+        // get the car model
+        public String getModel()
+        {
+            return modelName;
+        }
+
+        // get the car type
+        public String getType()
+        {
+            return carType;
+        }
+
+        // get the engine size
+        public double getSize()
+        {
+            return engineSize;
+        }
+
+        // get the sale price
+        public double getPrice()
+        {
+            return salePrice;
+        }
+
+        public int getSaleNumber()
+        {
+            return saleNumber;
+        }
+
+        public int getRecords()
+        {
+            return carCount;
+        }
+
+        public String toString()
+        {
+            return "Sale number: " + saleNumber + "\nMake: " + makeName
+            + "\nModel: " + modelName + "\nType: " + 
+        }
+
+        private int saleNumber;
+        private String makeName;
+        private String modelName;
+        private String carType;
+        private double engineSize;
+        private double salePrice;
+        private Dealership assignedDealership;
+
+    }// End Car class
--- CarSales.java
+++ CarSales.java
@@ -1,15 +1,12 @@
-class CarSales
+public class CarSales
 
     {
-        private String streetName;
-        private String cityName;
-        private String countyName;
-
-        private int
-
-
-        public CarSales()
+        public static void main(String args[])
         {
+           Address address1 = new Address("128 Milly View", "Portlaoise", "Laois");
 
+           Dealership dealership1 = new Dealership("Bills", address1);
+
+           
         }
     }
